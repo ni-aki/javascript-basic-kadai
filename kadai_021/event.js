e57de2c26ef111b628f48a6b2e8b52b53f81862d
ni-aki <helloakitty1213@yahoo.co.jp>
2025-07-09 20:33:50 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+//btnというidを持つHTMLを取得し、定数に代入する
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+// btnがクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+ setTimeout(() => {
+  text.textContent = "ボタンをクリックしました";
+}, 2000);
+
+});
